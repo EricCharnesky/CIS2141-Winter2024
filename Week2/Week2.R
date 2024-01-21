8e47cf2347c8ff770bf64e3a460336382d145b53
Eric Charnesky <EricCharnesky@gmail.com>
2024-01-21 02:32:25 -0500

    bread

--- Week2/Week2.R
+++ Week2/Week2.R
@@ -2,3 +2,6 @@
 gallons_of_milk_needed_per_day <- 6 * 1 / 16
 gallons_of_milk_needed_per_week <- gallons_of_milk_needed_per_day * 7
 
+
+loaves_of_white_bread_needed_per_day <- 6 * 2 / 18
+loaves_of_white_bread_needed_per_week <- loaves_of_white_bread_needed_per_day * 7
